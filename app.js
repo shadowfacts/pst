1c5adb1a3ca31f8b09d4be45b7b76c22e543545e
Shadowfacts <akorde@icloud.com>
2014-11-15 19:17:25 -0500

    Fix multiple mistakes

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 // Modules
 var express = require('express'),
 	logger = require('morgan'),
-	path = require('path'),
+	path = require('path');
 
 // App
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-express.use(logger('dev'));
+app.use(logger('dev'));
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-	res.render('index', {});
+	res.render('index', { title: 'pst' });
 });
