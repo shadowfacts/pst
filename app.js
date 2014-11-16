81acd4005b0f31ab6bf6df15eb74e3b4d5d3bdcc
Shadowfacts <akorde@icloud.com>
2014-11-15 19:12:44 -0500

    Add application skeleton to app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// pst
+// Modules
+var express = require('express'),
+	logger = require('morgan'),
+	path = require('path'),
+
+// App
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+express.use(logger('dev'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+	res.render('index', {});
+});
