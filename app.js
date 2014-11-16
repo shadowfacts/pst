25c70431c4f91fd6722904776c5591cf3539cc70
Shadowfacts <akorde@icloud.com>
2014-11-15 19:58:40 -0500

    Add favicon and static asset directory

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // Modules
 var express = require('express'),
 	logger = require('morgan'),
+	favicon = require('serve-favicon'),
 	path = require('path');
 
 // App
@@ -10,9 +11,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
 	res.render('index', { title: 'pst' });
 });
+
+console.log('pst application starting on port ' + app.get('port'));
+app.listen(app.get('port'));
